8a9a749e626a48d6944a40739545aae8edf2c8d6
faizz97 <fmshk97@gmail.com>
2018-09-29 17:57:21 +0530

    Modified FastExpo and Added MatrixExpo

--- Advanced/FastExponentiation.java
+++ Advanced/FastExponentiation.java
@@ -1,33 +1,34 @@
-/* Calculates x^y in O(log(y)) */
+package Advanced;
+/* Calculates constant ^ power & (constant ^ power) % mod in O(log(y)) */
 
 class FastExponentiation {
-	public double exp(double x, double y) { 	/* When y is negative, answer will be a double value */
-		if (y == 0 || x == 1) {
+	public double pow(double constant, double power) { 	/* When power is negative, answer will be a double value */
+		if (power == 0 || constant == 1) {
 			return 1d;
 		}
-		if (y == 1) {
-			return x;
+		if (power == 1) {
+			return constant;
 		}
-		if (x == 0) {
+		if (constant == 0) {
 			return 0d;
 		}
-		boolean isNegativeX = (x < 0)? true: false;
-		boolean isNegativeY = (y < 0)? true: false;
+		boolean isNegativeX = (constant < 0)? true: false;
+		boolean isNegativeY = (power < 0)? true: false;
 		if (isNegativeX) {
-			x *= -1;
+			constant *= -1;
 		}
 		if (isNegativeY) {
-			y *= -1;
+			power *= -1;
 		}
-		boolean isResultNegative = (isNegativeX && y % 2 == 1)? true: false;
+		boolean isResultNegative = (isNegativeX && power % 2 == 1)? true: false;
 		double result = 1d;
-		while (y > 0) {
-			if (y % 2 == 1) {
-				result *= x;
-				y--;
+		while (power > 0) {
+			if (power % 2 == 1) {
+				result *= constant;
+				power--;
 			}
-			x *= x;
-			y /= 2;
+			constant *= constant;
+			power /= 2;
 		}
 		if (isNegativeY) {
 			result = 1 / result;
@@ -35,30 +36,93 @@ class FastExponentiation {
 		return isResultNegative? (-1 * result): result;
 	}
 	
-	public long exp(long x, long y) {		/* x^y when y is positive */
-		if (y == 0 || x == 1) {
+	public long pow(long constant, long power) {		/* constant ^ power when power is positive */
+		if (power == 0 || constant == 1) {
 			return 1l;
 		}
-		if (y == 1) {
-			return x;
+		if (power == 1) {
+			return constant;
 		}
-		if (x == 0 || y < 0) {
+		if (constant == 0 || power < 0) {
 			return 0l;
 		}
-		boolean isNegativeX = (x < 0)? true: false;
+		boolean isNegativeX = (constant < 0)? true: false;
 		if (isNegativeX) {
-			x *= -1;
+			constant *= -1;
 		}
-		boolean isResultNegative = (isNegativeX && y % 2 == 1)? true: false;
+		boolean isResultNegative = (isNegativeX && power % 2 == 1)? true: false;
 		long result = 1l;
-		while (y > 0) {
-			if (y % 2 == 1) {
-				result *= x;
-				y--;
+		while (power > 0) {
+			if (power % 2 == 1) {
+				result *= constant;
+				power--;
 			}
-			x *= x;
-			y /= 2;
+			constant *= constant;
+			power /= 2;
 		}
 		return isResultNegative? (-1 * result): result;
+	}
+	
+	public double pow(double constant, double power, long mod) {
+		if (power == 0 || constant == 1) {
+			return 1d;
+		}
+		if (power == 1) {
+			return constant;
+		}
+		if (constant == 0) {
+			return 0d;
+		}
+		boolean isNegativeX = (constant < 0)? true: false;
+		boolean isNegativeY = (power < 0)? true: false;
+		if (isNegativeX) {
+			constant *= -1;
+		}
+		if (isNegativeY) {
+			power *= -1;
+		}
+		boolean isResultNegative = (isNegativeX && power % 2 == 1)? true: false;
+		double result = 1d;
+		constant %= mod;
+		while (power > 0) {
+			if (power % 2 == 1) {
+				result = (result * constant) % mod;
+				power--;
+			}
+			constant = (constant * constant) % mod;
+			power /= 2;
+		}
+		if (isNegativeY) {
+			result = 1 / result;
+		}
+		return isResultNegative? (result + mod): result;
+	}
+	
+	public long pow(long constant, long power, long mod) {
+		if (power == 0 || constant == 1) {
+			return 1l;
+		}
+		if (power == 1) {
+			return constant;
+		}
+		if (constant == 0 || power < 0) {
+			return 0l;
+		}
+		boolean isNegativeX = (constant < 0)? true: false;
+		if (isNegativeX) {
+			constant *= -1;
+		}
+		boolean isResultNegative = (isNegativeX && power % 2 == 1)? true: false;
+		long result = 1l;
+		constant %= mod;
+		while (power > 0) {
+			if (power % 2 == 1) {
+				result = (result * constant) % mod;
+				power--;
+			}
+			constant = (constant * constant) % mod;
+			power /= 2;
+		}
+		return isResultNegative? (mod + result): result;
 	}
 }
---
+++ Advanced/MatrixExponentiation.java
@@ -0,0 +1,77 @@
+package Advanced;
+/* Calculates matrix ^ power in O(log(power)). Matrix must be of size n x n. */
+
+class MatrixExponentiation {
+	public long[][] pow(long input[][], long power) {
+		long result[][] = new long[input.length][input[0].length];
+		if (power == 0) {
+			for (int i = 0; i < result.length; i++) {
+				result[i][i] = 1;
+			}
+			return result;
+		}
+		if (power == 1) {
+			return input;
+		}
+		result = input;
+		power--;
+		while (power > 0) {
+			if (power % 2 == 1) {
+				result = multiplyMatrix(result, input);
+				power--;
+			}
+			input = multiplyMatrix(input, input);
+			power /= 2;
+		}
+		return result;
+	}
+	public long[][] pow(long input[][], long power, long mod) {
+		long result[][] = new long[input.length][input[0].length];
+		if (power == 0) {
+			for (int i = 0; i < result.length; i++) {
+				result[i][i] = 1;
+			}
+			return result;
+		}
+		if (power == 1) {
+			return input;
+		}
+		result = input;
+		power--;
+		while (power > 0) {
+			if (power % 2 == 1) {
+				result = multiplyMatrix(result, input, mod);
+				power--;
+			}
+			input = multiplyMatrix(input, input, mod);
+			power /= 2;
+		}
+		return result;
+	}
+	private long[][] multiplyMatrix(long matrix1[][], long matrix2[][]) {
+		long result[][] = new long[matrix1[0].length][matrix2.length];
+		
+	    for (int i = 0; i < matrix1[0].length; i++) {
+	        for (int j = 0; j < matrix1[0].length; j++) {
+	            result[i][j] = 0;
+	            for (int k = 0; k < matrix1[0].length; k++) {
+	            	result[i][j] = result[i][j] + (matrix1[i][k] * matrix2[k][j]);
+	            }
+	        }
+	    }
+		return result;
+	}
+	private long[][] multiplyMatrix(long matrix1[][], long matrix2[][], long mod) {
+		long result[][] = new long[matrix1[0].length][matrix2.length];
+		
+	    for (int i = 0; i < matrix1[0].length; i++) {
+	        for (int j = 0; j < matrix1[0].length; j++) {
+	            result[i][j] = 0;
+	            for (int k = 0; k < matrix1[0].length; k++) {
+	            	result[i][j] = (result[i][j] + (matrix1[i][k] * matrix2[k][j])) % mod;
+	            }
+	        }
+	    }
+		return result;
+	}
+}
